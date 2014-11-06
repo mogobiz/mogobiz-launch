2fb727ecc758144304cb72edf8e1373397fef17b
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2014-11-06 16:47:44 +0100

    no message

--- src/main/scala/com/mogobiz/launch/run/RestAll.scala
+++ src/main/scala/com/mogobiz/launch/run/RestAll.scala
@@ -1,13 +1,13 @@
-package com.mogobiz.launch
+package com.mogobiz.launch.run
 
 import akka.actor.Props
 import akka.io.IO
-import com.mogobiz.actors.{ActorSystemLocator, MogobizActors}
-import com.mogobiz.config.Settings
-import com.mogobiz.jobs.CleanCartJob
-import com.mogobiz.mail.EmailService
 import com.mogobiz.pay.config.{MogopayActors, MogopayRoutes}
-import com.mogobiz.services.MogobizRoutes
+import com.mogobiz.run.actors.{ActorSystemLocator, MogobizActors}
+import com.mogobiz.run.config.Settings
+import com.mogobiz.run.jobs.CleanCartJob
+import com.mogobiz.run.mail.EmailService
+import com.mogobiz.run.services.MogobizRoutes
 import com.mogobiz.system.{BootedMogobizSystem, RoutedHttpService}
 import spray.can.Http
 
