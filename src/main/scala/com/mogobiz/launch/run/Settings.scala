4986e18b5c7e8504505343891b7cb3f6c3d3537c
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2014-11-10 15:22:39 +0100

    handle fallback

--- src/main/scala/com/mogobiz/launch/run/Settings.scala
+++ src/main/scala/com/mogobiz/launch/run/Settings.scala
@@ -7,7 +7,7 @@ package com.mogobiz.launch.run
 import com.typesafe.config.ConfigFactory
 
 object Settings {
-  private val config = ConfigFactory.load()
+  private val config = ConfigFactory.load().withFallback(ConfigFactory.load("default-application"))
 
   val Interface = config.getString("spray.can.server.interface")
   val Port = config.getInt("spray.can.server.port")
